d3a2592e1017811cd4493a41eb0ea7fc344c0691
sl251 <197351806+sl251@users.noreply.github.com>
2026-06-28 01:28:48 +0800

    chore: bump version to 0.3.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.gameocr.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.3.5"
+        versionCode = 9
+        versionName = "0.3.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
